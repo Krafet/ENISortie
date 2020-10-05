72423047e992df1c5163d2591c71479fd02f9e3b
canbolat-o <delasalle.sio.canbolat.o@gmail.com>
2020-10-05 18:44:38 +0200

    Création des classes et des repository

---
+++ src/Entity/Campus.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CampusRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CampusRepository::class)
+ */
+class Campus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $no_campus;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nom_campus;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNoCampus(): ?int
+    {
+        return $this->no_campus;
+    }
+
+    public function setNoCampus(int $no_campus): self
+    {
+        $this->no_campus = $no_campus;
+
+        return $this;
+    }
+
+    public function getNomCampus(): ?string
+    {
+        return $this->nom_campus;
+    }
+
+    public function setNomCampus(string $nom_campus): self
+    {
+        $this->nom_campus = $nom_campus;
+
+        return $this;
+    }
+}
